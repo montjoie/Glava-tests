84ae4c1925c8450fa4ab5f414df0417403c28ba4
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-04-02 13:01:37 +0200

    Split network out of kselftests

--- kselftests.sh
+++ kselftests.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-# TODO move this in a dedicated test
-# assume only one network card
-ETH=$(ls /sys/class/net |grep -E 'eth|enp')
-
-if [ -z "$ETH" ];then
-	echo "WARN: no network card found"
-else
-	echo "INFO: configure network card $ETH"
-	ln -s /etc/init.d/net.lo /etc/init.d/net.$ETH
-	/etc/init.d/net.$ETH start
-fi
-
 if [ ! -e /root/kselftest.tar.gz ];then
 	case $(uname -m) in
 	aarch64)
